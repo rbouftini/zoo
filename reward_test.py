2e99b55eea1fcbc332c5f2b00f0d49f4ce68fc16
Rayane Bouftini <rbouftin@iwu.edu>
2025-10-12 21:51:51 +0100

    pin cpu

--- reward_test.py
+++ reward_test.py
@@ -4,9 +4,9 @@ from datasets import load_dataset
 from torch.utils.data import DataLoader
 import time
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model_name = "Skywork/Skywork-Reward-V2-Qwen3-0.6B"
-batch_size = 2
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+model_name = "Skywork/Skywork-Reward-V2-Llama-3.1-8B"
+batch_size = 128
 
 rm = AutoModelForSequenceClassification.from_pretrained(
     model_name,
@@ -19,10 +19,10 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 def collate(batch):
     batch = tokenizer.apply_chat_template(batch, tokenize=False, add_generation_prompt=False)
-    batch = tokenizer(batch, return_tensors="pt", padding=True).to(device)
+    batch = tokenizer(batch, return_tensors="pt", padding=True)
     return batch
 
-dataset = load_dataset("HuggingFaceH4/ultrafeedback_binarized", split="test_prefs[:10]")
+dataset = load_dataset("HuggingFaceH4/ultrafeedback_binarized", split="test_prefs")
 n_examples = dataset.num_rows
 
 chosen_loader = DataLoader(dataset["chosen"], batch_size=batch_size, collate_fn=collate, shuffle=False, pin_memory=True)
@@ -30,12 +30,16 @@ rejected_loader = DataLoader(dataset["rejected"], batch_size=batch_size, collate
 
 n_w = 0
 for a_w, a_l in zip(chosen_loader, rejected_loader):
+   a_w = {k: v.to(device, non_blocking=True) for k, v in a_w.items()} 
+   a_l = {k: v.to(device, non_blocking=True) for k, v in a_l.items()} 
+
    time_start = time.time()
    with torch.no_grad():
     scores_w = rm(**a_w).logits
     scores_l = rm(**a_l).logits
     n_w += (scores_w > scores_l).long().sum(0).item()
 
+   torch.cuda.synchronize()
    time_end = time.time()
    print(f"Time taken: {(time_end - time_start):.4f}")
 
