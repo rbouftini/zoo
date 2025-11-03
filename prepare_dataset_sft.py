f0dbee3865f9305d3c053168ecdc449c609906b4
Rayane Bouftini <rbouftin@iwu.edu>
2025-11-03 19:00:04 +0100

    add conversational dataset

--- prepare_dataset_sft.py
+++ prepare_dataset_sft.py
@@ -6,40 +6,55 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 model_name = "Qwen/Qwen3-0.6B-Base"
 dataset_name = "HuggingFaceTB/smol-smoltalk"
-flag = "<think>\n\n</think>\n"
-N_TRAIN_EX = 50000
-N_TEST_EX = 500
+N_TRAIN_EX = 100000
+N_TEST_EX = 5000
 
 dataset = load_dataset(dataset_name)
 dataset = dataset.remove_columns("source")
-dataset = dataset.shuffle(seed=7)
+dataset = dataset.shuffle(seed=21)
 
 dataset["train"] = dataset["train"].select(range(min(N_TRAIN_EX, dataset["train"].num_rows)))
 dataset["test"] = dataset["test"].select(range(min(N_TEST_EX, dataset["test"].num_rows)))
 
 def format(row):
-    # For now, we format our dataset only as prompt and completion (not conversational)
-    if row["messages"][0]["role"] == "system":
-        row["messages"] = row["messages"][:3]
-    else:
-        row["messages"] = row["messages"][:2]
+    messages = row["messages"]
+    # Messages is a list of dictionaries for the conversation
+    # We will return the tokenized conversation and mask for the tokens to account for in the SFT loss.
+    # Mask==0 for user or system content, else its 1
+    input_ids, mask = [], []
+    def add_tokens(ids, mask_val):
+        if isinstance(ids, int):
+            ids = [ids]
+        input_ids.extend(ids)
+        mask.extend([mask_val] * len(ids))
 
-    full_text = tokenizer.apply_chat_template(row["messages"], add_generation_prompt=False, enable_thinking=False, tokenize=False)
-    prompt_len = len(tokenizer(full_text[:full_text.find(flag)])["input_ids"])
-    input_ids = tokenizer(full_text, return_tensors="pt")["input_ids"]
-
-    input_ids = input_ids.tolist()[0]
+    # Sometimes the conversation starts with a system prompt
+    if messages[0]["role"] == "system":
+        msg = "<|im_start|>system\n" + messages[0]["content"] + "<|im_end|>\n"
+        ids = tokenizer.encode(msg)
+        add_tokens(ids, 0)
+        messages = messages[1:]
+    
+    for message in messages:
+        if message["role"] == "user":
+            msg = "<|im_start|>user\n" + message["content"] + "<|im_end|>\n"
+            ids = tokenizer.encode(msg)
+            add_tokens(ids, 0)
+        else:
+            assert message["role"] == "assistant"
+            msg =  "<|im_start|>assistant\n<think>\n\n</think>\n\n" + message["content"] + "<|im_end|>\n"
+            ids = tokenizer.encode(msg)
+            add_tokens(ids, 1)
 
     return {
         "input_ids": input_ids,
-        "prompt_len": prompt_len
+        "mask": mask
     }
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 dataset = dataset.map(format, remove_columns=dataset["train"].column_names, num_proc=16)
-print(dataset)
 
-dataset = dataset.filter(lambda ex: len(ex["input_ids"]) <=512, num_proc=16)
+dataset = dataset.filter(lambda ex: len(ex["input_ids"])<=1024, num_proc=16)
 print(dataset)
 
 dataset.save_to_disk("../smoltalkIds")
